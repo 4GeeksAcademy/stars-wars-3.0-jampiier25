7a477bd02576b6d4e6f326453c821fa01771ff90
Jean Pierre Pluas <jampiier@gmail.com>
2025-04-04 15:53:49 +0000

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
-import { StoreProvider } from "./hooks/useGlobalReducer"; // Asegura que esté bien importado
+import { StoreProvider } from "./hooks/useGlobalReducer"; 
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FavoritesProvider } from "./context/FavoritesContext";
 
